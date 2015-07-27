4611d9343e493a1371bfd9aae5d4f5767f608303
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2015-07-27 05:22:59 +0200

    Imported Upstream version 0.1.13~alpha8

--- src/lib/clxx/cl/program_lazy_generator.cpp
+++ src/lib/clxx/cl/program_lazy_generator.cpp
@@ -14,10 +14,9 @@ program_lazy_generator::
 program_lazy_generator()
   : program_generator(),
    _lazy_get_program(
-       make_memoized_function(
-         &clxx::program_generator::get_program, 
-         static_cast<program_generator const*>(this)
-       )
+       [this](clxx::context const& context) -> clxx::program {
+          return this->program_generator::get_program(context);
+       }
    )
 { }
 /* ----------------------------------------------------------------------- */
